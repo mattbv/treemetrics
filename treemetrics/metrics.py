9c4a37093a5951c856f3e98d5e2f23b3c94987b1
Matheus Boni Vicari <matheus_boni_vicari@hotmail.com>
2017-08-29 09:54:39 +0100

    Added new metric to calculate the average of groups in a pandas.DataSet. Added temporary functions to fit inverse log functions to data grouped by branch order.

--- treemetrics/metrics.py
+++ treemetrics/metrics.py
@@ -10,3 +10,7 @@ import numpy as np
 def cylinder_radius(volume, length):
     
     return np.sqrt(volume / (np.pi * length))
+
+
+def group_avg(df, var_name):
+    return df.groupby([var_name], as_index=False).mean()
--- treemetrics/qsm_analysis.py
+++ treemetrics/qsm_analysis.py
@@ -8,7 +8,11 @@ import numpy as np
 import seaborn as sns
 from qsm_parser import read_mat
 from metrics import cylinder_radius
+from metrics import group_avg
 import matplotlib.pyplot as plt
+import numpy.polynomial.polynomial as poly
+from numpy.polynomial import Polynomial
+from scipy.optimize import curve_fit
 
 
 def radius_order_analysis(mat_file):
@@ -35,14 +39,61 @@ def volume_order_analysis(mat_file):
     fig = plt.figure()
     fig = sns.boxplot(x='Branch order', y='Volume',  data=branch_data)
     fig.set(xlabel='Branch order', ylabel='Volume (m^3)')
+
+
+def fit_radius_order(mat_file):
     
+    mat_data = read_mat(mat_file)
+    
+    r = cylinder_radius(mat_data['BVol'],  mat_data['BLen'])
+
+    branch_data = np.hstack((r, mat_data['BOrd']))
+    branch_data = pd.DataFrame(branch_data, columns=['Radius', 'Branch order'])
+
+    radius_avg = group_avg(branch_data, 'Branch order')
+    
+    x = np.asarray(radius_avg['Branch order']).astype(float)
+    y = np.asarray(radius_avg['Radius']).astype(float)
+    popt, pcov = curve_fit(func, x, y, p0=(1, 1, 1))
+    xx = np.arange(0, np.max(x) + 1, 1)
+    yy = func(xx, *popt)
+
+    plt.plot(xx, yy)
+    
+    return popt
+
+
+def fit_volume_order(mat_file):
+    
+    mat_data = read_mat(mat_file)
+    
+    branch_data = np.hstack((mat_data['BVol'], mat_data['BOrd']))
+    branch_data = pd.DataFrame(branch_data, columns=['Volume', 'Branch order'])
+
+    radius_avg = group_avg(branch_data, 'Branch order')
+    
+    x = np.asarray(radius_avg['Branch order']).astype(float)
+    y = np.asarray(radius_avg['Volume']).astype(float)
+    popt, pcov = curve_fit(func, x, y, p0=(1, 1, 1))
+    xx = np.arange(0, np.max(x) + 1, 1)
+    yy = func(xx, *popt)
+
+    plt.plot(xx, yy)
+    
+    return popt
+
+
+def func(x, a, c, d):
+    return a*np.exp(-c*x)+d
 
 
 if __name__ == "__main__":
     
     mat_file = '../data/r6_22_testQSM.mat'
     radius_order_analysis(mat_file)
+    fit_radius_order(mat_file)    
     volume_order_analysis(mat_file)
+#    fit_volume_order(mat_file)
 
 #plt.figure()
 #data1 = np.hstack((mat['BVol'], mat['BOrd']))
